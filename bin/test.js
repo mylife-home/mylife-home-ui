73a48c946e8e575b5914f23bc210f7b051a4fdcb
Vincent Trumpff <vincent@stoic.com>
2015-10-03 21:46:40 +0200

    admin client

--- bin/test.js
+++ bin/test.js
@@ -1,14 +1,21 @@
-const Net = require('../lib/net');
+'use strict';
+
+const async = require('async');
+const Net   = require('../lib/net');
+const Admin = require('../lib/admin');
 
 const netConfig = {
-  host : 'rpi2-devel.mti-team2.dyndns.org',
-  port : 6667
+  host          : 'rpi2-devel.mti-team2.dyndns.org',
+  port          : 6667,
+  ui_channel    : 'mylife-ui',
+  admin_channel : 'mylife-admin'
 };
 
-var client = new Net.Client(netConfig, 'test-js', 'mylife-ui');
+var netClient = new Net.Client(netConfig, 'test-js');
+var adminClient = new Admin.Client(netConfig, 'test-js-admin');
 
-process.on('SIGINT', function() {
-  client.close(function() {
-    process.exit();
-  });
-});
+process.on('SIGINT', () => async.parallel([
+    (cb) => netClient.close(cb),
+    (cb) => adminClient.close(cb)
+  ],
+  () => process.exit()));
