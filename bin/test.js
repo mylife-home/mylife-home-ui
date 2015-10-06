94508ed55318392668acb05c4488d8aefb1fd656
Vincent Trumpff <vincent@stoic.com>
2015-10-06 23:18:30 +0200

    main server

--- bin/test.js
+++ bin/test.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const async = require('async');
-const Net   = require('../lib/net');
-const Admin = require('../lib/admin');
-const Web   = require('../lib/web');
+const Server = require('../lib/server');
 
 const netConfig = {
   host           : 'rpi2-devel.mti-team2.dyndns.org',
@@ -13,38 +10,6 @@ const netConfig = {
   resources_nick : 'mylife-home-resources'
 };
 
-const adminDefinition = {
-  test: {
-    desc: 'Test desc',
-    impl: (w) => w('test!')
-  },
-  toto: {
-    desc: 'Toto desc',
-    children: {
-      subToto: {
-        desc: 'Sub toto desc',
-        impl: (w, m) => w('sub toto => ' + m),
-      }
-    }
-  }
-};
+var server = new Server(netConfig, 8001);
 
-function sessionCreator(socket) {
-  console.log('connected');
-  socket.on('disconnect', () => console.log('disconnected'));
-  socket.on('message', (data) => console.log('data:', data));
-}
-
-var netClient = new Net.Client(netConfig, 'test-js');
-var netRepository = new Net.Repository(netClient);
-var netJPacketManager = new Net.JPacketManager(netClient);
-
-var adminClient = new Admin.Client(netConfig, 'test-js-admin', adminDefinition);
-var webServer = new Web.Server(netRepository, netJPacketManager, sessionCreator, 8001);
-
-process.on('SIGINT', () => async.parallel([
-    (cb) => webServer.close(cb),
-    (cb) => netClient.close(cb),
-    (cb) => adminClient.close(cb)
-  ],
-  () => process.exit()));
+process.on('SIGINT', () => server.close(() => process.exit()));
