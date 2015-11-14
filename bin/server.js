0d88e5f8770820a5922d5da10b733e2a60933743
Vincent Trumpff <vincent@stoic.com>
2015-11-14 23:12:14 +0100

    log init

--- bin/server.js
+++ bin/server.js
@@ -1,13 +1,15 @@
 'use strict';
 
-const path = require('path');
-
+const path   = require('path');
+const common = require('mylife-home-common');
 const Server = require('../lib/server');
 
 let configFile = path.join(__dirname, '../conf/config.json');
 if(process.argv[2]) { configFile = process.argv[2]; }
 configFile = path.resolve(configFile);
 const config = require(configFile);
+
+common.logging.init(config);
 
 const server = new Server(config);
 
