38f55962979f53012b711cae65fc3cde8e13ff4e
Vincent Trumpff <vincent@stoic.com>
2016-01-05 10:33:04 +0100

    socket.io client access path fix

--- public/system.config.js
+++ public/system.config.js
@@ -8,6 +8,6 @@ System.config({
     'angular-route': 'bower_components/angular-route/angular-route.js',
     'angular-bootstrap': 'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
     'async': 'bower_components/async/dist/async.js',
-    'socket.io-client': 'bower_components/socket.io-client/socket.io-client-1.3.7/socket.io.js'
+    'socket.io-client': 'bower_components/socket.io-client/socket.io.js'
   }
 });
