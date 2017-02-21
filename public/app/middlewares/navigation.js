65ea688ab7a876bf464d530944913b6d26f55fa7
Vincent Trumpff <vincent@stoic.com>
2017-02-21 18:05:15 +0100

    better navigation

--- public/app/middlewares/navigation.js
+++ public/app/middlewares/navigation.js
@@ -7,7 +7,6 @@ const middleware = (/*store*/) => next => action => {
 
   switch (action.type) {
     case '@@router/LOCATION_CHANGE': {
-      if(action.payload.action !== 'PUSH') { return; }
       let { pathname } = action.payload;
       pathname = pathname.substr(1);
       return pathname && next(windowNavigationChange(pathname));
